3b371e92aac97ed59bfee5bddcd9ae6b0be9ef1c
kdeng00 <kundeng00@pm.me>
2025-08-28 16:50:34 -0400

    Added function call to update queued song status

--- src/managers/commit_manager.rs
+++ src/managers/commit_manager.rs
@@ -458,9 +458,14 @@ impl CommitManager {
                 .link_queued_song_to_queued_coverart(token, &queued_song_id, &id)
                 .await
             {
-                Ok(_) => {
-                    println!("Queued coverart Id: {id:?}");
-                    println!("Linked queued song to queued coverart");
+                Ok(_) => match up.update_queued_song_status(token, &queued_song_id, "ready").await {
+                    Ok(_) => {
+                        println!("Queued coverart Id: {id:?}");
+                        println!("Linked queued song to queued coverart");
+                    }
+                    Err(err) => {
+                        return Err(std::io::Error::other(err.to_string()));
+                    }
                 }
                 Err(err) => {
                     return Err(std::io::Error::other(err.to_string()));
