11b8cc21d33484099783220086198642cacc4e2f
KD <kundeng00@pm.me>
2025-08-30 13:06:04 -0400

    Delete song (#67)
    * Changed code to comply with async
    
    * Able to delete song
    
    * Updated readme
    
    * Code formatting and cleanup
    
    * Version bump

--- src/managers/commit_manager.rs
+++ src/managers/commit_manager.rs
@@ -5,7 +5,6 @@ use std::io::{Result, Write};
 use std::str::FromStr;
 
 use serde::{Deserialize, Serialize};
-use tokio::runtime::Runtime;
 
 use crate::managers;
 use crate::models::{self};
@@ -169,11 +168,9 @@ impl CommitManager {
 
         println!("Deleting song..");
 
-        let res_fut = del.delete_song(&token, &song);
-        let result = Runtime::new().unwrap().block_on(res_fut);
-        match result {
-            Ok(o) => {
-                println!("Song (Id {:?}) has been successfully deleted", o.id);
+        match del.delete_song(&token, &song).await {
+            Ok((song, _coverart)) => {
+                println!("Song (Id {:?}) has been successfully deleted", song.id);
             }
             Err(er) => {
                 println!("Error {er:?}");
