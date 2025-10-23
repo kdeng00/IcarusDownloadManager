3cadac4efd225161312d6ad8b1d4258750fe1ec6
KD <kundeng00@pm.me>
2025-10-23 16:52:26 -0400

    tsk-78: Updating returned error on converting ostring to string (#87)
    * tsk-78: Making error to get string from o string more robust
    
    * tsk-78: Version bump

--- src/utilities/string.rs
+++ src/utilities/string.rs
@@ -1,6 +1,20 @@
 pub fn o_to_string(val: &std::ffi::OsString) -> Result<std::string::String, std::io::Error> {
     match val.clone().into_string() {
         Ok(value) => Ok(value),
-        Err(_) => Err(std::io::Error::other(String::from("Error"))),
+        Err(err) => match err.into_string() {
+            Ok(res) => {
+                Err(std::io::Error::other(res.to_string()))
+            }
+            Err(err) => {
+                match err.to_str() {
+                    Some(err) => {
+                        Err(std::io::Error::other(err))
+                    }
+                    None => {
+                        Err(std::io::Error::other("Undefined error"))
+                    }
+                }
+            }
+        }
     }
 }
